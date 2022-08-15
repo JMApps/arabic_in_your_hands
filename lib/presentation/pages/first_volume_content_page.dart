42c6486375848fcbadb7a87b115153b949362051
jmapps <jmappsmuslim@gmail.com>
2022-08-15 10:40:42 +0300

    Added share app link's button

--- lib/presentation/pages/first_volume_content_page.dart
+++ lib/presentation/pages/first_volume_content_page.dart
@@ -1,12 +1,14 @@
 import 'package:arabicinyourhands/presentation/lists/first_volume_chapter_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 
 class FirstVolumeContentPage extends StatelessWidget {
   const FirstVolumeContentPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Том 1'),
@@ -14,6 +16,20 @@ class FirstVolumeContentPage extends StatelessWidget {
           scale: 0.7,
           child: Image.asset('assets/icons/arabic_in_origin.png'),
         ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              CupertinoIcons.share,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Share.share(
+                'Арабский перед тобой\n\nВерсия iOS:\nhttps://apps.apple.com/ru/app/арабский-перед-тобой/id1602988060\n\nВерсия android:\nhttps://play.google.com/store/apps/details?id=jmapps.arabicinyourhands',
+                sharePositionOrigin: Rect.fromLTWH(0, 0, size.width, size.height / 2),
+              );
+            },
+          ),
+        ],
       ),
       body: CupertinoScrollbar(
         child: FirstVolumeChapterList(),
--- lib/presentation/pages/second_volume_content_page.dart
+++ lib/presentation/pages/second_volume_content_page.dart
@@ -1,12 +1,14 @@
 import 'package:arabicinyourhands/presentation/lists/second_volume_chapter_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 
 class SecondVolumeContentPage extends StatelessWidget {
   const SecondVolumeContentPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Том 2'),
@@ -14,6 +16,20 @@ class SecondVolumeContentPage extends StatelessWidget {
           scale: 0.7,
           child: Image.asset('assets/icons/arabic_in_origin.png'),
         ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              CupertinoIcons.share,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Share.share(
+                'Арабский перед тобой\n\nВерсия iOS:\nhttps://apps.apple.com/ru/app/арабский-перед-тобой/id1602988060\n\nВерсия android:\nhttps://play.google.com/store/apps/details?id=jmapps.arabicinyourhands',
+                sharePositionOrigin: Rect.fromLTWH(0, 0, size.width, size.height / 2),
+              );
+            },
+          ),
+        ],
       ),
       body: CupertinoScrollbar(
         child: SecondVolumeChapterList(),
