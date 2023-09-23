7edd9fc37a4feaed0fd0a5bad3cff3f07ea57662
dev_muslim <jmappsmuslim@gmail.com>
2023-09-23 08:06:28 +0300

    Added all Domain and Data's classes

---
+++ lib/data/repositories/secondVolume/second_vol_sub_chapters_data_repository.dart
@@ -0,0 +1,35 @@
+import 'package:arabicinyourhands/data/datasources/services/database_helper.dart';
+import 'package:arabicinyourhands/data/models/secondVolume/second_vol_sub_chapter_model.dart';
+import 'package:arabicinyourhands/domain/entities/secondVolume/second_vol_sub_chapter_entity.dart';
+import 'package:arabicinyourhands/domain/repositories/seconVolume/second_vol_sub_chapter_repository.dart';
+import 'package:sqflite/sqflite.dart';
+
+class SecondVolSubChaptersDataRepository implements SecondVolSubChaptersRepository {
+
+  final DatabaseHelper _databaseHelper = DatabaseHelper();
+
+  @override
+  Future<List<SecondVolSubChapterEntity>> getSecondVolSubChapters({required String tableName}) async {
+    final Database database = await _databaseHelper.db;
+    List<Map<String, Object?>> resources = await database.query(tableName);
+    List<SecondVolSubChapterEntity> secondVolumeSubChapters = resources.isNotEmpty ? resources.map((c) => _mapToEntity(SecondVolSubChapterModel.fromMap(c))).toList() : [];
+    return secondVolumeSubChapters;
+  }
+
+  @override
+  Future<List<SecondVolSubChapterEntity>> getSecondVolSubChaptersById({required String tableName, required int chapterId}) async {
+    final Database database = await _databaseHelper.db;
+    List<Map<String, Object?>> resources = await database.query(tableName, where: 'by_chapter_id = $chapterId');
+    List<SecondVolSubChapterEntity> secondVolumeChaptersById = resources.isNotEmpty ? resources.map((c) => _mapToEntity(SecondVolSubChapterModel.fromMap(c))).toList() : [];
+    return secondVolumeChaptersById;
+  }
+
+  // Mapping to entity
+  SecondVolSubChapterEntity _mapToEntity(SecondVolSubChapterModel model) {
+    return SecondVolSubChapterEntity(
+      id: model.id,
+      dialogTitle: model.dialogTitle,
+      dialogSubTitle: model.dialogSubTitle,
+    );
+  }
+}
