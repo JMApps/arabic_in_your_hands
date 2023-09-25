cba6b1d81c57f007c5fc54b2427e43f61072e194
dev_muslim <jmappsmuslim@gmail.com>
2023-09-25 08:56:33 +0300

    Added two volumes flip pages

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -2,11 +2,11 @@ import 'package:arabicinyourhands/data/database/model/dictionary_category_argume
 import 'package:arabicinyourhands/data/database/model/dictionary_words_flip_arguments.dart';
 import 'package:arabicinyourhands/domain/arguments/first_sub_chapter_args.dart';
 import 'package:arabicinyourhands/domain/arguments/second_sub_chapter_args.dart';
-import 'package:arabicinyourhands/presentation/pages/userDictionary/dictionary_words_page.dart';
+import 'package:arabicinyourhands/presentation/pages/firstVolume/first_vol_contents_flip_page.dart';
 import 'package:arabicinyourhands/presentation/pages/firstVolume/first_vol_contents_page.dart';
-import 'package:arabicinyourhands/presentation/pages/firstVolume/first_volume_content_flip_mode_page.dart';
+import 'package:arabicinyourhands/presentation/pages/seconVolume/second_vol_contents_flip_page.dart';
 import 'package:arabicinyourhands/presentation/pages/seconVolume/second_vol_contents_page.dart';
-import 'package:arabicinyourhands/presentation/pages/seconVolume/second_volume_content_flip_mode_page.dart';
+import 'package:arabicinyourhands/presentation/pages/userDictionary/dictionary_words_page.dart';
 import 'package:arabicinyourhands/presentation/pages/userDictionary/words_content_flip_mode_page.dart';
 import 'package:flutter/material.dart';
 
@@ -17,25 +17,40 @@ class AppRoutes {
         final FirstSubChapterArgs firstSubChapterArgs =
             routeSettings.arguments as FirstSubChapterArgs;
         return MaterialPageRoute(
-            builder: (_) => FirstVolContentsPage(
-                firstVolSubChapterModel: firstSubChapterArgs.model),
-            settings: routeSettings);
-      case '/first_volume_content_flip':
+          builder: (_) => FirstVolContentsPage(
+            firstVolSubChapterEntity: firstSubChapterArgs.model,
+          ),
+          settings: routeSettings,
+        );
+      case '/first_vol_contents_flip_page':
+        final FirstSubChapterArgs firstSubChapterArgs =
+            routeSettings.arguments as FirstSubChapterArgs;
         return MaterialPageRoute(
-            builder: (_) => FirstVolumeContentFlipModePage(),
-            settings: routeSettings);
+          builder: (_) => FirstVolContentsFlipPage(
+            firstVolSubChapterEntity: firstSubChapterArgs.model,
+          ),
+          settings: routeSettings,
+        );
       case '/second_vol_contents_page':
         final SecondSubChapterArgs secondubChapterArgs =
             routeSettings.arguments as SecondSubChapterArgs;
         return MaterialPageRoute(
-            builder: (_) => SecondVolContentsPage(
-                secondVolSubChapterModel: secondubChapterArgs.model),
-            settings: routeSettings);
-      case '/second_volume_content_flip':
+          builder: (_) => SecondVolContentsPage(
+            secondVolSubChapterEntity: secondubChapterArgs.model,
+          ),
+          settings: routeSettings,
+        );
+      case '/second_vol_contents_flip_page':
+        final SecondSubChapterArgs secondubChapterArgs =
+            routeSettings.arguments as SecondSubChapterArgs;
         return MaterialPageRoute(
-            builder: (_) => SecondVolumeContentFlipModePage(),
-            settings: routeSettings);
-        // User words
+          builder: (_) => SecondVolContentsFlipPage(
+            secondVolSubChapterEntity: secondubChapterArgs.model,
+          ),
+          settings: routeSettings,
+        );
+
+      // User words
       case '/category_words_content':
         final DictionaryCategoryArguments dictionaryCategoryArguments =
             routeSettings.arguments as DictionaryCategoryArguments;
---
+++ lib/presentation/pages/seconVolume/second_vol_contents_flip_page.dart
@@ -0,0 +1,65 @@
+import 'package:arabicinyourhands/core/state/provider/flip_page_state.dart';
+import 'package:arabicinyourhands/core/styles/app_styles.dart';
+import 'package:arabicinyourhands/core/themes/app_theme.dart';
+import 'package:arabicinyourhands/domain/entities/secondVolume/second_vol_sub_chapter_entity.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class SecondVolContentsFlipPage extends StatelessWidget {
+  const SecondVolContentsFlipPage({
+    super.key,
+    required this.secondVolSubChapterEntity,
+  });
+
+  final SecondVolSubChapterEntity secondVolSubChapterEntity;
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme appColors = Theme.of(context).colorScheme;
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => FlipPageState(),
+        ),
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(secondVolSubChapterEntity.dialogTitle),
+          backgroundColor: appColors.primaryColor,
+          actions: [
+            IconButton(
+              icon: const Icon(
+                CupertinoIcons.arrow_2_circlepath,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                // Flip card
+              },
+            ),
+          ],
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Card(
+              margin: AppStyles.mainMardingMini,
+              shape: AppStyles.mainShape,
+              color: appColors.inversePrimary,
+              child: Padding(
+                padding: AppStyles.mainMarding,
+                child: Text(
+                  secondVolSubChapterEntity.dialogSubTitle,
+                  style: const TextStyle(
+                    fontSize: 18,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
