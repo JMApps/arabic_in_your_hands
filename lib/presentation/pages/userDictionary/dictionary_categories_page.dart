5f5ea2ad3cb0fefb99b8068c974b2eded1bf00d9
dev_muslim <jmappsmuslim@gmail.com>
2023-09-27 20:16:57 +0300

    Added dictionary categories list

--- lib/presentation/pages/userDictionary/dictionary_categories_page.dart
+++ lib/presentation/pages/userDictionary/dictionary_categories_page.dart
@@ -1,6 +1,7 @@
-import 'package:arabicinyourhands/presentation/pages/userDictionary/dictionary_categories_list.dart';
-import 'package:arabicinyourhands/presentation/pages/userDictionary/add_category_popup.dart';
-import 'package:arabicinyourhands/presentation/pages/userDictionary/search_category_delegate.dart';
+import 'package:arabicinyourhands/core/themes/app_theme.dart';
+import 'package:arabicinyourhands/presentation/pages/userDictionary/lists/dictionary_categories_list.dart';
+import 'package:arabicinyourhands/presentation/pages/userDictionary/widgets/add_category_popup.dart';
+import 'package:arabicinyourhands/presentation/widgets/main_app_icon.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -10,60 +11,44 @@ class DictionaryCategoriesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Builder(
-      builder: (context) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text(AppLocalizations.of(context)!.categories),
-            leading: Transform.scale(
-              scale: 0.7,
-              child: Image.asset('assets/icons/arabic_in_origin.png'),
-            ),
-            actions: [
-              IconButton(
-                splashRadius: 20,
-                icon: const Icon(
-                  CupertinoIcons.search,
-                ),
-                onPressed: () {
-                  showSearch(
-                    context: context,
-                    delegate: SearchCategoryDelegate(),
-                  );
-                },
-              ),
-            ],
-          ),
-          body: const DictionaryCategoriesList(),
-          floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-          floatingActionButton: FloatingActionButton(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: const Icon(
-              CupertinoIcons.add,
-              color: Colors.white,
-            ),
+    final AppLocalizations locale = AppLocalizations.of(context)!;
+    final ColorScheme appColors = Theme.of(context).colorScheme;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(locale.categories),
+        backgroundColor: appColors.primaryDark,
+        leading: const MainAppIcon(),
+        actions: [
+          IconButton(
+            icon: const Icon(CupertinoIcons.search),
             onPressed: () {
-              showModalBottomSheet(
-                backgroundColor: Colors.transparent,
-                isScrollControlled: true,
-                context: context,
-                builder: (context) {
-                  return SingleChildScrollView(
-                    child: AnimatedPadding(
-                      padding: MediaQuery.of(context).viewInsets,
-                      duration: const Duration(milliseconds: 250),
-                      curve: Curves.decelerate,
-                      child: const AddCategoryPopup(),
-                    ),
-                  );
-                },
-              );
+
             },
           ),
-        );
-      },
+        ],
+      ),
+      body: const CupertinoScrollbar(
+        child: DictionaryCategoriesList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(CupertinoIcons.add),
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            isScrollControlled: true,
+            builder: (context) {
+              return SingleChildScrollView(
+                child: AnimatedPadding(
+                  padding: MediaQuery.of(context).viewInsets,
+                  duration: const Duration(milliseconds: 250),
+                  curve: Curves.decelerate,
+                  child: const AddCategoryPopup(),
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
---
+++ lib/presentation/pages/userDictionary/lists/dictionary_categories_list.dart
@@ -0,0 +1,71 @@
+import 'package:arabicinyourhands/core/styles/app_styles.dart';
+import 'package:arabicinyourhands/data/repositories/userDictionary/user_dictionary_category_data_repository.dart';
+import 'package:arabicinyourhands/domain/entities/userDictionary/user_dictionary_category_entity.dart';
+import 'package:arabicinyourhands/domain/usecases/usetDictionary/user_dictionary_categories_use_case.dart';
+import 'package:arabicinyourhands/presentation/pages/userDictionary/items/dictionary_category_item.dart';
+import 'package:arabicinyourhands/presentation/widgets/error_data_text.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+
+class DictionaryCategoriesList extends StatefulWidget {
+  const DictionaryCategoriesList({Key? key}) : super(key: key);
+
+  @override
+  State<DictionaryCategoriesList> createState() => _DictionaryCategoriesListState();
+}
+
+class _DictionaryCategoriesListState extends State<DictionaryCategoriesList> {
+  late final UserDictionaryCategoriesUseCase _categoriesUseCase;
+
+  @override
+  void initState() {
+    _categoriesUseCase = UserDictionaryCategoriesUseCase(UserDictionaryCategoryDataRepository.getInstance());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final AppLocalizations locale = AppLocalizations.of(context)!;
+    return FutureBuilder<List<UserDictionaryCategoryEntity>>(
+      future: _categoriesUseCase.fetchWordCategories(),
+      builder: (BuildContext context, AsyncSnapshot<List<UserDictionaryCategoryEntity>> snapshot) {
+        if (snapshot.hasData) {
+          return AnimationLimiter(
+            child: ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final UserDictionaryCategoryEntity model = snapshot.data![index];
+                return AnimationConfiguration.staggeredList(
+                  position: index,
+                  duration: const Duration(milliseconds: 250),
+                  child: SlideAnimation(
+                    verticalOffset: 150,
+                    child: FadeInAnimation(
+                      child: DictionaryCategoryItem(
+                        item: model,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return ErrorDataText(error: snapshot.error.toString());
+        } else {
+          return Center(
+            child: Padding(
+              padding: AppStyles.mainMarding,
+              child: Text(
+                locale.dictionary_category_add_first,
+                style: const TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
