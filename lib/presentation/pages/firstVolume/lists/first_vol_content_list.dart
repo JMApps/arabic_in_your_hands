9fc8ef05351e91aafbce3507d177fc72d3b0cf12
dev_muslim <jmappsmuslim@gmail.com>
2023-10-16 18:27:40 +0300

    Added scrollable positioned listview

--- lib/presentation/pages/firstVolume/lists/first_vol_content_list.dart
+++ lib/presentation/pages/firstVolume/lists/first_vol_content_list.dart
@@ -4,10 +4,12 @@ import 'package:arabicinyourhands/domain/entities/firstVolume/first_vol_content_
 import 'package:arabicinyourhands/domain/usecases/firstVolume/first_vol_contents_use_case.dart';
 import 'package:arabicinyourhands/presentation/pages/firstVolume/items/first_vol_content_item_left.dart';
 import 'package:arabicinyourhands/presentation/pages/firstVolume/items/first_vol_content_item_right.dart';
+import 'package:arabicinyourhands/presentation/uiState/player/content_player_state.dart';
 import 'package:arabicinyourhands/presentation/widgets/error_data_text.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class FirstVolContentsList extends StatefulWidget {
   const FirstVolContentsList({super.key, required this.firstSubChapterId});
@@ -20,6 +22,7 @@ class FirstVolContentsList extends StatefulWidget {
 
 class _FirstVolContentsListState extends State<FirstVolContentsList> {
   late final FirstVolContentsUseCase _firstVolContentsUseCase;
+  final ItemScrollController _contentController = ItemScrollController();
 
   @override
   void initState() {
@@ -30,30 +33,40 @@ class _FirstVolContentsListState extends State<FirstVolContentsList> {
   @override
   Widget build(BuildContext context) {
     final AppLocalizations? locale = AppLocalizations.of(context);
+    final int trackIndex = Provider.of<ContentPlayerState>(context).getCurrentTrackIndex;
     return FutureBuilder<List<FirstVolContentEntity>>(
       future: _firstVolContentsUseCase.fetchFirstContentsById(
         tableName: locale!.tableNameFirstVolContents,
         firstSubChapterId: widget.firstSubChapterId,
       ),
-      builder: (BuildContext context, AsyncSnapshot<List<FirstVolContentEntity>> snapshot) {
+      builder: (BuildContext context,
+          AsyncSnapshot<List<FirstVolContentEntity>> snapshot) {
         if (snapshot.hasData) {
-          return CupertinoScrollbar(
-            child: ListView.builder(
-              padding: AppStyles.mainMardingHorizontalMini,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (BuildContext context, index) {
-                final FirstVolContentEntity model = snapshot.data![index];
-                return index.isOdd
-                    ? FirstVoContentItemLeft(
-                  model: model,
-                  index: index,
-                )
-                    : FirstVolContentItemRight(
-                  model: model,
-                  index: index,
-                );
-              },
-            ),
+          if (_contentController.isAttached) {
+            _contentController.scrollTo(
+              index: trackIndex,
+              alignment: 0.5,
+              curve: Curves.bounceIn,
+              duration: const Duration(milliseconds: 5),
+            );
+          }
+          return ScrollablePositionedList.builder(
+            physics: const ClampingScrollPhysics(),
+            itemScrollController: _contentController,
+            padding: AppStyles.mainMardingHorizontalMini,
+            itemCount: snapshot.data!.length,
+            itemBuilder: (BuildContext context, index) {
+              final FirstVolContentEntity model = snapshot.data![index];
+              return index.isOdd
+                  ? FirstVoContentItemLeft(
+                      model: model,
+                      index: index,
+                    )
+                  : FirstVolContentItemRight(
+                      model: model,
+                      index: index,
+                    );
+            },
           );
         } else if (snapshot.hasError) {
           return ErrorDataText(error: snapshot.error.toString());
@@ -66,4 +79,3 @@ class _FirstVolContentsListState extends State<FirstVolContentsList> {
     );
   }
 }
-
--- lib/presentation/pages/seconVolume/lists/second_vol_content_list.dart
+++ lib/presentation/pages/seconVolume/lists/second_vol_content_list.dart
@@ -4,9 +4,12 @@ import 'package:arabicinyourhands/domain/entities/secondVolume/second_vol_conten
 import 'package:arabicinyourhands/domain/usecases/seconVolume/second_vol_contents_use_case.dart';
 import 'package:arabicinyourhands/presentation/pages/seconVolume/items/second_vol_content_item_left.dart';
 import 'package:arabicinyourhands/presentation/pages/seconVolume/items/second_vol_content_item_right.dart';
+import 'package:arabicinyourhands/presentation/uiState/player/content_player_state.dart';
 import 'package:arabicinyourhands/presentation/widgets/error_data_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class SecondVolContentList extends StatefulWidget {
   const SecondVolContentList({super.key, required this.secondSubChapterId});
@@ -19,37 +22,50 @@ class SecondVolContentList extends StatefulWidget {
 
 class _SecondVolContentListState extends State<SecondVolContentList> {
   late final SecondVolContentsUseCase _secondVolContentsUseCase;
+  final ItemScrollController _contentController = ItemScrollController();
 
   @override
   void initState() {
     super.initState();
-    _secondVolContentsUseCase = SecondVolContentsUseCase(SecondVolContentsDataRepository());
+    _secondVolContentsUseCase =
+        SecondVolContentsUseCase(SecondVolContentsDataRepository());
   }
 
   @override
   Widget build(BuildContext context) {
-    final AppLocalizations locale = AppLocalizations.of(context)!;
+    final AppLocalizations? locale = AppLocalizations.of(context);
+    final int trackIndex = Provider.of<ContentPlayerState>(context).getCurrentTrackIndex;
     return FutureBuilder<List<SecondVolContentEntity>>(
       future: _secondVolContentsUseCase.fetchSecondContentsById(
-        tableName: locale.tableNameSecondVolContents,
+        tableName: locale!.tableNameSecondVolContents,
         secondSubChapterId: widget.secondSubChapterId,
       ),
       builder: (BuildContext context, AsyncSnapshot<List<SecondVolContentEntity>> snapshot) {
         if (snapshot.hasData) {
-          return ListView.builder(
+          if (_contentController.isAttached) {
+            _contentController.scrollTo(
+              index: trackIndex,
+              alignment: 0.5,
+              curve: Curves.bounceIn,
+              duration: const Duration(milliseconds: 5),
+            );
+          }
+          return ScrollablePositionedList.builder(
+            physics: const ClampingScrollPhysics(),
+            itemScrollController: _contentController,
             padding: AppStyles.mainMardingHorizontalMini,
             itemCount: snapshot.data!.length,
             itemBuilder: (BuildContext context, index) {
               final SecondVolContentEntity model = snapshot.data![index];
               return index.isOdd
                   ? SecondVolContentItemLeft(
-                model: model,
-                index: index,
-              )
+                      model: model,
+                      index: index,
+                    )
                   : SecondVolContentItemRight(
-                model: model,
-                index: index,
-              );
+                      model: model,
+                      index: index,
+                    );
             },
           );
         } else if (snapshot.hasError) {
@@ -63,4 +79,3 @@ class _SecondVolContentListState extends State<SecondVolContentList> {
     );
   }
 }
-
